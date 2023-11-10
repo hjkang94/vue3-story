5ca8951b9f22739373ef25e599a6909c30d9cefe
hjkang94 <hjkang94@github.com>
2023-11-10 13:25:51 +0900

    vue3 프로젝트 생성, 초기 컴포넌트 생성

---
+++ src/App.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="header">
+    <ul class="header-button-left">
+      <li v-if="step !== 0" @click="step--">취소</li>
+    </ul>
+    <ul class="header-button-right">
+      <li v-if="step === 1" @click="step++">다음</li>
+      <li v-if="step === 2" @click="publish">발행</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo" />
+  </div>
+
+  <PostContainer
+    @write="content = $event"
+    :step="step"
+    :data="data"
+    :imageUrl="imageUrl"
+  />
+
+  <button v-if="step === 0" @click="more">더보기</button>
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input
+        @change="upload"
+        accept="image/*"
+        type="file"
+        id="file"
+        class="inputfile"
+      />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
+</template>
+
+<script>
+import PostContainer from "./components/PostContainer.vue";
+import data from "./assets/data.js";
+import axios from "axios";
+
+export default {
+  name: "App",
+
+  components: {
+    PostContainer,
+  },
+
+  data() {
+    return {
+      data,
+      count: 0,
+      step: 0,
+      imageUrl: "",
+      content: "",
+    };
+  },
+
+  methods: {
+    more() {
+      axios
+        .get(`https://codingapple1.github.io/vue/more${this.count}.json`)
+        .then((res) => {
+          this.data.push(res.data);
+          this.count++;
+        });
+    },
+
+    upload(event) {
+      const files = event.target.files;
+      const file = files[0];
+      if (!file.type.includes("image")) {
+        return alert("이미지 파일을 선택해주세요.");
+      }
+      this.imageUrl = URL.createObjectURL(files[0]);
+      this.step++;
+    },
+
+    publish() {
+      const post = {
+        name: "강현지",
+        userImage: "https://picsum.photos/100?random=3",
+        postImage: this.imageUrl,
+        likes: 36,
+        date: "May 15",
+        liked: false,
+        content: this.content,
+        filter: "perpetua",
+      };
+      this.data.unshift(post);
+      this.step = 0;
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+.logo {
+  width: 22px;
+  margin: auto;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
+}
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
+  text-align: center;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+.inputfile {
+  display: none;
+}
+.input-plus {
+  cursor: pointer;
+}
+#app {
+  box-sizing: border-box;
+  font-family: "consolas";
+  margin-top: 60px;
+  width: 100%;
+  max-width: 460px;
+  margin: auto;
+  position: relative;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
+}
+</style>
---
+++ src/components/FilterBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="filter-item"
+    :class="filter"
+    :style="{ backgroundImage: `url(${imageUrl})` }"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: "FilterBox",
+
+  props: {
+    imageUrl: String,
+    filter: String,
+  },
+};
+</script>
+
+<style>
+.filter-item {
+  width: 100px;
+  height: 100px;
+  margin: 10px 10px 10px auto;
+  padding: 8px;
+  display: inline-block;
+  color: white;
+  background-size: cover;
+  background-position: center;
+}
+</style>
